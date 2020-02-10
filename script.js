0abf3f5c0d34438bd6fb7aab1bc9965adbfba818
Sui.Young <suisauce.codes@gmail.com>
2020-02-09 22:01:37 -0500

    added script for Jquery CDN, and included script for my JS file.

--- script.js
+++ script.js
@@ -4,13 +4,3 @@ $(document).ready(function () {
 });
 
 
-// Find out which keyCode is with which key: http://keycode.info/
-
-// document.addEventListener("keydown", function (e) {
-//     let keyCode = e.keyCode;
-//     let audio = document.querySelector("audio[id='" + keyCode + "']");
-//     console.log(audio);
-//     audio.play();
-//     audio.currentTime = 0;
-// })
-
