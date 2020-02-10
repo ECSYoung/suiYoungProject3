004090b680f24aec2f4cb5cc4b3079c14759c7f5
Sui.Young <suisauce.codes@gmail.com>
2020-02-09 21:41:15 -0500

    final basic design done.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+// document ready
+$(document).ready(function () {
+    console.log('ready');
+});
+
+
 // Find out which keyCode is with which key: http://keycode.info/
 
-document.addEventListener("keydown", function (e) {
-    let keyCode = e.keyCode;
-    let audio = document.querySelector("audio[id='" + keyCode + "']");
-    console.log(audio);
-    audio.play();
-    audio.currentTime = 0;
-})
+// document.addEventListener("keydown", function (e) {
+//     let keyCode = e.keyCode;
+//     let audio = document.querySelector("audio[id='" + keyCode + "']");
+//     console.log(audio);
+//     audio.play();
+//     audio.currentTime = 0;
+// })
 
