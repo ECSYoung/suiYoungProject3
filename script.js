3adbbd065514c196e3d1ddcb9bf02ab6cf31b170
Sui.Young <suisauce.codes@gmail.com>
2020-02-12 16:40:34 -0500

    inserting text and adjusted bottom margin of span class

--- script.js
+++ script.js
@@ -52,81 +52,203 @@ tarotCard = {
             </p>`,
             advice: `
             <h5>Advice:</h5>
-            <p><span class="pBreak">
+            <p><span class="pBreak">Have faith in your innate creativity. The Magician advises you give your questioning nature and free-associating mind plenty of room to explore the subject at hand. Behave just as if you were an open-minded and curious scientist. Through this process, you may bring freshness and clarity into the situation that is both stimulating and catalytic.</span>
+
+            <span class="pBreak">You do not have to understand it all intellectually. Besides, intuition is your ace in the hole. Respond in a spontaneous manner to what is right in front of you. There is no reason to hold yourself back. Your natural urges are exactly what is needed, and, your ingenuous timing and elegant style will help smooth over any awkwardness.<span>
             
             </p>`
         },
         {//array index: 2
-            card: `<img class = "cardFront" src="./Assets/Cards/wingedSpirit/2.jpg" alt="The Fool Card">`,
-            title: `<h4 class="pBreak">The Fool</h4>`,
-            generalMeaning: `header`,
-            advice: `p text`
+            card: `<img class = "cardFront" src="./Assets/Cards/wingedSpirit/2.jpg" alt="High Priestess">`,
+            title: `<h4 class="pBreak">High Priestess</h4>`,
+            generalMeaning: `
+            <h5>General Meaning:</h5>
+            <p><span class="pBreak">The High Priestess is a Major Arcana, or "trump" card, that represents human wisdom. The High Priestess can be viewed as a kind of female Pope (a Papess), or the ancient Egyptian Priestess of Isis, the even more ancient snake and bird goddesses, the Greek goddess Persephone, or Eve, before the fall.</span>
+
+            <span class="pBreak">For the accused heretics who were burnt at the stake for revering her in the 14th and 15th century, the Priestess symbolized the prophecy of the return of the Holy Spirit, which was perceived as the female aspect of the Holy Trinity.</span>
+
+            <span class="pBreak">In terms of the Major Arcana ordering of cards, The High Priestess appears in the sequence as soon as the Fool decides he wants to develop his innate powers, making a move toward becoming a Magician. The High Priestess is his first teacher, representing the inner life and the method for contacting it, as well as the contemplative study of nature and spiritual mystery.</span>
+            
+            </p>`,
+            advice: `
+            <h5>Advice:</h5>
+            <p><span class="pBreak">The High Priestess advises you to adhere to your chosen spiritual practice on a more regular basis. If you want the benefits of evolution, you'll have to cooperate with spirit. We all have distractions, demands -- a whole life full of reasons why we cannot find the time to retreat into our inner sanctum.</span>
+
+            <span class="pBreak">Until you consider this as vital to your well-being as the need to eat and sleep, you are likely to be eternally restless and deeply dissatisfied. A spiritual routine that suits your temperament, practiced every day, is the most trustworthy path to freedom.</span>
+            
+            </p>`
         },
         {//array index: 3
             card: `<img class = "cardFront" src="./Assets/Cards/wingedSpirit/3.jpg" alt="The Empress">`,
             title: `<h4 class="pBreak">The Empress</h4>`,
-            generalMeaning: `header`,
-            advice: `p text`
+            generalMeaning: `
+            <h5>General Meaning:</h5>
+            <p><span class="pBreak">The Empress is a Major Arcana, or "trump" card, that portrays the energy of the great mother. She is nature, around us but also within us, the ever-unfolding source of life-giving power. The Empress is often pictured as a pre-Christian Goddess, as the one whom The High Priestess is channeling down to Earth for the rest of us.</span>
+
+            <span class="pBreak">In medieval Europe, The Empress card was painted to represent whatever queen currently ruled the land, probably to satisfy the Inquisitors. But the scholars of the Renaissance and beyond had no doubt of her true identity -- she could not be fully revealed on Tarot cards as "the woman clothed with the sun" until after the French Revolution.</span>
+
+            <span class="pBreak">This supreme archetype of femininity also symbolizes fertility. It is The Empress who provides us nourishment and security. She is also sometimes seen as delighting us with flowers and fruit. A potentially terrifying aspect of this archetype manifests itself whenever karmic mood swings wipe out our plans, like a storm that has come upon us. Whatever happens, The Empress is the source of our embodiment and of natural lLaw. She might even be called "The Great Recycler."</span>
+            </p>`,
+            advice: `
+            <h5>Advice:</h5>
+            <p><span class="pBreak">The Empress advises you to trust in the good sense you have shown up to this point. Recognize your good intentions in carrying out your responsibilities as a compassionate human. You are capable of demonstrating the finest aspects of your personality.</span>
+
+            <span class="pBreak">Demonstrate this through caring actions, a forgiving and generous attitude, and wise understanding of others' needs and struggles. Bring a healing influence to the current situation and take full credit for the supportive part you play.</span>
+            
+            
+            </p>`
         },
         {//array index: 4
             card: `<img class = "cardFront" src="./Assets/Cards/wingedSpirit/4.jpg" alt="The Emperor">`,
             title: `<h4 class="pBreak">The Emperor</h4>`,
-            generalMeaning: `header`,
-            advice: `p text`
+            generalMeaning: `
+            <h5>General Meaning:</h5>
+            <p><span class="pBreak">In the most practical terms, The Emperor Tarot card represents the highest leadership, a head of state, or the most exemplary and powerful person in the realm. This archetypal ruler is responsible for the affairs of a society or community, which are directly proportional to his well being and happiness.</span>
+
+            <span class="pBreak">The more enlightenment and cosmic perspective this energy brings, the better life is for all. The Emperor archetype masters the world of matter and physical manifestation. When you apply this card to your situation, acknowledge your potentials for mastery. Reinforce a sense of sovereignty within yourself, despite any self-limiting beliefs, habits, or appearances to the contrary.</span>
+            
+            </p>`,
+            advice: `
+            <h5>Advice:</h5>
+            <p><span class="pBreak">When the Emperor arises, he advises you get in touch with your inner sovereignty and natural self-possession. Realize that somewhere deep inside you is the memory of royalty. Reach within to find that strain of natural nobility and leadership. These innate qualities will help you manage your current situation.</span>
+
+            <span class="pBreak">The Emperor suggests that you have the necessary abilities to be the final authority. This situation is an opportunity to showcase your competence and skill. Act with the confidence of someone who knows how to take care of business.</span>
+            
+            </p>`
         },
         {//array index: 6
             card: `<img class = "cardFront" src="./Assets/Cards/wingedSpirit/5.jpg" alt="The Hierophant">`,
             title: `<h4 class="pBreak">The Hierophant</h4>`,
-            generalMeaning: `header`,
-            advice: `p text`
+            generalMeaning: `
+            <h5>General Meaning:</h5>
+            <p><span class="pBreak">The Hierophant Tarot this card refers to a master and the learning of practical lessons from the study of natural law. This energy of this card points to some agent or resource that can reveal the secrets of life, the cycles of the Moon and tides, the links between human beings and the heavens.</span>
+
+            <span class="pBreak">Because monasteries were the only places a person could learn to read and write in the Middle Ages, a Hierophant was one to whom a student would petition for entry. He was the one to set the curriculum for the neophyte's course of study.</span>
+
+            <span class="pBreak">Often pictured with the right hand raised in blessing, the Hierophant is linked with the ancient lineage of Melchezidek, initiator of the Hebrew priestly tradition, the one who passes on the teachings. All shamans of any tradition draw upon this archetype.</span>
+            </p>`,
+            advice: `
+            <h5>Advice:</h5>
+            <p><span class="pBreak">The Hierophant advises that you return to the role of a meticulous student. Learn everything you can about your chosen area. Let that knowledge become a part of you and an operative influence on your day-to-day awareness. In this way, you can slowly and steadily establish real credibility in your field or chosen subject.<span>
+
+            <span class="pBreak">Earn respect and recognition by completing your education and broadening your experience. If you already have all the necessary experience you need, then rewrite your resume so others can appreciate who you are and what you can bring to a situation. Focus on your goal and be determined. You may be destined to be a master in your realm.</span>
+            </p>`
         },
         {//array index: 7
             card: `<img class = "cardFront" src="./Assets/Cards/wingedSpirit/6.jpg" alt="The Lovers">`,
             title: `<h4 class="pBreak">The Lovers</h4>`,
-            generalMeaning: `header`,
-            advice: `p text`
+            generalMeaning: `
+            <h5>General Meaning:</h5>
+            <p><span class="pBreak"></span>
+            
+            </p>`,
+            advice: `
+            <h5>Advice:</h5>
+            <p><span class="pBreak"></span>
+            
+            </p>`
         },
         {//array index: 8
             card: `<img class = "cardFront" src="./Assets/Cards/wingedSpirit/7.jpg" alt="The Chariot">`,
             title: `<h4 class="pBreak">The Chariot</h4>`,
-            generalMeaning: `header`,
-            advice: `p text`
+            generalMeaning: `
+            <h5>General Meaning:</h5>
+            <p><span class="pBreak">Although it has taken on a strictly romantic revision of meaning in some modern decks, traditionally The Lovers Tarot card reflected the challenges of choosing a partner. At a crossroads, one cannot take both paths. The images on this card in different decks have varied more than most, because we have had so many ways of looking at sex and relationships across cultures and centuries.</span>
+
+            <span class="pBreak">Classically, the energy of this card reminded us of the real challenges posed by romantic relationships, with the protagonist often shown in the act of making an either-or choice. To partake of a higher ideal often requires sacrificing the lesser option. The path of pleasure eventually leads to distraction from spiritual growth. The gratification of the personality eventually gives way to a call from spirit as the soul matures.</span>
+
+            <span class="pBreak">Modern decks tend to portray the feeling of romantic love with this card, showing Adam and Eve at the gates of Eden when everything was still perfect. This interpretation portrays humanity before the fall, and can be thought to imply a different sort of choice -- the choice of evolution over perfection, or the choice of personal growth through relationship -- instead of a fantasy where everything falls into place perfectly and is taken care of without effort.</span>
+            
+            </p>`,
+            advice: `
+            <h5>Advice:</h5>
+            <p><span class="pBreak"></span>
+            
+            </p>`
         },
         {//array index: 9
             card: `<img class = "cardFront" src="./Assets/Cards/wingedSpirit/8.jpg" alt="The Strength">`,
             title: `<h4 class="pBreak">The Strength</h4>`,
-            generalMeaning: `header`,
-            advice: `p text`
+            generalMeaning: `
+            <h5>General Meaning:</h5>
+            <p><span class="pBreak"></span>
+            
+            </p>`,
+            advice: `
+            <h5>Advice:</h5>
+            <p><span class="pBreak"></span>
+            
+            </p>`
         },
         {//array index: 10
             card: `<img class = "cardFront" src="./Assets/Cards/wingedSpirit/9.jpg" alt="The Hermit">`,
             title: `<h4 class="pBreak">The Hermit</h4>`,
-            generalMeaning: `header`,
-            advice: `p text`
+            generalMeaning: `
+            <h5>General Meaning:</h5>
+            <p><span class="pBreak"></span>
+            
+            </p>`,
+            advice: `
+            <h5>Advice:</h5>
+            <p><span class="pBreak"></span>
+            
+            </p>`
         },
         {//array index: 11
             card: `<img class = "cardFront" src="./Assets/Cards/wingedSpirit/10.jpg" alt="The World">`,
             title: `<h4 class="pBreak">The World</h4>`,
-            generalMeaning: `header`,
-            advice: `p text`
+            generalMeaning: `
+            <h5>General Meaning:</h5>
+            <p><span class="pBreak"></span>
+            
+            </p>`,
+            advice: `
+            <h5>Advice:</h5>
+            <p><span class="pBreak"></span>
+            
+            </p>`
         },
         {//array index: 12
             card: `<img class = "cardFront" src="./Assets/Cards/wingedSpirit/11.jpg" alt="The Justice">`,
             title: `<h4 class="pBreak">The Justice</h4>`,
-            generalMeaning: `header`,
-            advice: `p text`
+            generalMeaning: `
+            <h5>General Meaning:</h5>
+            <p><span class="pBreak"></span>
+            
+            </p>`,
+            advice: `
+            <h5>Advice:</h5>
+            <p><span class="pBreak"></span>
+            
+            </p>`
         },
         {//array index: 13
             card: `<img class = "cardFront" src="./Assets/Cards/wingedSpirit/12.jpg" alt="The Hanged Man">`,
             title: `<h4 class="pBreak">The Hanged Man</h4>`,
-            generalMeaning: `header`,
-            advice: `p text`
+            generalMeaning: `
+            <h5>General Meaning:</h5>
+            <p><span class="pBreak"></span>
+            
+            </p>`,
+            advice: `
+            <h5>Advice:</h5>
+            <p><span class="pBreak"></span>
+            
+            </p>`
         },
         {//array index: 14
             card: `<img class = "cardFront" src="./Assets/Cards/wingedSpirit/13.jpg" alt="Death">`,
             title: `<h4 class="pBreak">Death</h4>`,
-            generalMeaning: `header`,
-            advice: `p text`
+            generalMeaning: `
+            <h5>General Meaning:</h5>
+            <p><span class="pBreak"></span>
+            
+            </p>`,
+            advice: `
+            <h5>Advice:</h5>
+            <p><span class="pBreak"></span>
+            
+            </p>`
         },
         {//array index: 15
             card: `<img class = "cardFront" src="./Assets/Cards/wingedSpirit/14.jpg" alt="Temperance">`,
@@ -656,8 +778,8 @@ $('.deck7').on('click', function () {
         console.log('4. suiteIndex: ', suiteIndex); // ✔
         console.log('5. findSuite: ', findSuite); // ✔
         console.log('6. majorArcana:', tarotCard.majorArcana);
-    console.log('7. major Arcana Title:', tarotCard.majorArcana[majArcana].title, tarotCard.majorArcana[majArcana].card ); // ✔
-    console.log('8. findSuite[suiteIndex]:', tarotCardSuite[tarotSuiteIndex].title, tarotCardSuite[tarotSuiteIndex].card); // ✔
+        console.log('7. major Arcana Title:', tarotCard.majorArcana[majArcana].title, tarotCard.majorArcana[majArcana].card ); // ✔
+        console.log('8. findSuite[suiteIndex]:', tarotCardSuite[tarotSuiteIndex].title, tarotCardSuite[tarotSuiteIndex].card); // ✔
         console.log(`9. non maj arcana: ${tarotCardSuite[tarotSuiteIndex].card}`); // ✔
         console.log(`10. maj arcana: ${tarotCard.majorArcana[majArcana].card}`); // ✔
         console.log("11. tarotCardSuite: ", tarotCardSuite);
